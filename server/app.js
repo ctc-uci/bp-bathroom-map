915255fc175b3f4587516a23b4e1a7a3e2179354
Thien <ntmthien01@gmail.com>
2022-02-13 12:09:23 -0800

    revert all

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+//this is thien's branch - original
 const express = require('express');
 const cors = require('cors');
 
@@ -16,3 +17,34 @@ app.use(
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+/* 
+
+//////////////////////////////// SKELETON //////////////////////////////////
+- connect to MongoDB + create table structure
+    + bathroom table:
+        - rating
+        - availability
+        - type of bathrooms (gender inclusive, only male, only female, both?)
+    + tags table:
+        - tag name
+        - bathroom that the tag belongs to
+    + review:
+        - review's content
+        - bathroom that the review is for
+- handle HTTP request from client:
+    + Needs + wants:
+        1/set up client's frontend with:
+          - front end files (HTML, CSS, JS, etc.)
+          - list of bathrooms with the following infos for each:
+            + rating 
+            + review 
+            + availability (hours of service?)
+            + tags
+            + type of bathrooms (gender inclusive, only male, only female, both?)
+        2/adding review to MongoDB
+        3/recalculate rating in MongoDB
+    + Nice to have (I think this will require user to have account):
+        1/ favorite bathroom list
+        2/ bathroom emergency ping
+        3/ bathroom traffic (how many spots does it have left) ---------> last in priority (complicated)
+*/
