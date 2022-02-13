bf2cae1bad41ed7aa67d94a80dd5a55d3e3ab9b8
Zubair Sidhu <sidhuzubair@gmail.com>
2022-02-12 23:13:01 -0800

    Add comments to app.js, fix minor stlye issues with yarn format

--- server/app.js
+++ server/app.js
@@ -2,29 +2,31 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-
-// Routes (backend address for front end to reach)
-// ex const testRoute = require('./routes/testRoute');
+// Importing routes
+// Routes define how the server will respond to client requests
+// Read more about routing here: https://expressjs.com/en/starter/basic-routing.html
 const bathroomRoute = require('./routes/bathrooms');
 
+// Grab configuration variables from the .env file
 require('dotenv').config();
 
+// Create a new express app, and set its port
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
 // Connect to MongoDB
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 // Display this when mongoose successfully connects to the database
-mongoose.connection.on("open", function(){
-  console.log("mongodb is connected!!");
+mongoose.connection.on('open', function () {
+  console.log('mongodb is connected!!');
 });
 
+// Specify CORS origin
+// Read more about CORS here: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 app.use(
   cors({
     origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
@@ -37,13 +39,11 @@ app.use(
   cors({ credentials: true, origin: true }),
 );
 
-// Add routes here
-// ex: app.use(routeName);
+// Adding routes (imported above)
 // All requests arrive at App.js so now App.js redirects us to the right route
+// Any request to '/bathrooms' will be sent to bathroomRoute to be handled
 app.use('/bathrooms', bathroomRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-
