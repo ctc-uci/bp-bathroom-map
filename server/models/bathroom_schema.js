9269f0545d86e09715a044e8839325210cf93dcc
Zubair Sidhu <sidhuzubair@gmail.com>
2022-02-12 22:56:02 -0800

    Change _id type to ObjectId, add useful info about mongoose schemas

--- server/models/bathroom_schema.js
+++ server/models/bathroom_schema.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-// Populate fields for the Bathroom model
+// Read more about mongoose schemas here: https://mongoosejs.com/docs/guide.html
+// Read more about valid types here: https://mongoosejs.com/docs/schematypes.html
+
+// Populate field names and types for the Bathroom model
 const bathroomSchema = new mongoose.Schema({
-  _id: String,
+  _id: mongoose.Schema.ObjectId,
   name: String,
   latitude: Number,
   longitude: Number,
