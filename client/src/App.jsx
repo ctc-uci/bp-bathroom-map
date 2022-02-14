3e530850aa9fc339257f074dfdf7f2350fc7389a
Kyle Doliente <kdolient@uci.edu>
2022-02-13 21:14:26 -0800

    Starts application at user's location; shows bathroom locations with markers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+  DirectionsRenderer,
+} from "@react-google-maps/api"
+import ClosestBathroomButton from './components/ClosestBathroomButton';
+import { useState, useEffect } from 'react'
+import restrooms from "./assets/restrooms.json"
+import axios from "axios"
+
+/*
+
+right now, the application starts at the user's location.
+however, it's kind of inaccurate right now so we can't really
+rely on it to give walking directions yet.
+
+*/
+
 
 function App() {
+  // start the application by centering the map on the user's location
+  useEffect(() => {
+    navigator.geolocation.watchPosition((position) => {
+      setLat(position.coords.latitude)
+      setLng(position.coords.longitude)
+      console.log(lat,lng)
+    })
+  })
+
+  // load markers from backend
+  const [lat, setLat] = useState(33.64592727868923)
+  const [lng, setLng] = useState(-117.84273979898299)
+
+  // set dimensions of map
+  const mapContainerStyle = {
+    width: "100vw",
+    height: "90vh"
+  }
+
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey : process.env.REACT_APP_MAPS_API_KEY,
+  });
+
+  if(loadError) return "Error loading maps"
+
+  if(!isLoaded) return "Loading maps"
+
+  /*
+
+  kind of a lot to unpack here, but the <GoogleMap> tag renders
+  the map that we created with the specifications we added.
+  i recommend taking a look at the map function in react as well, as
+  it's useful for saving space.
+
+  */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <GoogleMap mapContainerStyle={mapContainerStyle} zoom = {16} center = {{lat:lat, lng:lng}}>
+        {restrooms.restrooms.map((item)=>(
+          <Marker
+            key={item.id}
+            position={{lat:item.latitude, lng:item.longitude}}
+          />
+        ))}
+      </GoogleMap>
+      <ClosestBathroomButton></ClosestBathroomButton>
     </div>
   );
 }
---
+++ client/src/components/ClosestBathroomButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './closestBathroomButton.css'
+
+const ClosestBathroomButton = () => {
+  return (
+    <button class="bathroom-button bathroom-button-text">Find Closest Restroom</button>
+  )
+}
+
+export default ClosestBathroomButton
