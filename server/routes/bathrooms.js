28128c9efa3844a51a57752ff7b3cd1625d8432c
Thien <ntmthien01@gmail.com>
2022-02-13 17:16:24 -0800

    import code

--- server/routes/bathrooms.js
+++ server/routes/bathrooms.js
@@ -9,15 +9,15 @@ const router = express.Router();
 // HTTP commands to send to back-end with given router address
 
 // Create a route to get all bathroom data from backend
-// req=HTTP request from frontend
-// res=HTTP respond for backend to populate and send to frontend
-// 200=success
-// 300=redirect
-// 404=not found
-// 500=error
+//    req=HTTP request from frontend
+//    res=HTTP respond for backend to populate and send to frontend
+//          200=success
+//          300=redirect
+//          404=not found
+//          500=error
 router.get('/', async (req, res) => {
   try {
-    const filter = {}; // Condition for object to meet
+    const filter = {}; // Condition for object to meet "{}"="all"
     const mongoResponse = await Bathroom.find(filter); // Select those that met the condition
     res.status(200).send(mongoResponse); // sending status and response to the frontend
   } catch (err) {
