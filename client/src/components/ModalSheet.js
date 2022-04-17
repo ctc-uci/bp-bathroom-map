52958527415a27b2b2c5d1fe5547bc08351a22b3
jrock-3 <justin.liao3@gmail.com>
2022-04-17 16:42:36 -0700

    added search results bathroom cards

--- client/src/components/ModalSheet.js
+++ client/src/components/ModalSheet.js
@@ -3,39 +3,53 @@ import './ModalSheet.css';
 import Sheet from 'react-modal-sheet';
 import Axios from 'axios';
 import Fuse from 'fuse.js';
+import SearchCard from './SearchCard.js';
 
 const ModalSheet = () => {
   const [isOpen, setOpen] = useState(false);
 
-  const [bathrooms, setBathrooms]=useState('');
+  const options= { distance:20, findAllMatches:true, limit:10 };
+  const [fuse, setFuse] = useState();
   const [bathroomResults, setBathroomResults]=useState([]);
   const [bathroomsList, setBathroomsList] = useState([]);
-
-  const [fuse, setFuse] = useState();
-
-  const options= { distance:20, findAllMatches:true, limit:10 };
+  const [bathroomsJSON, setBathroomsJSON] = useState({});
 
   const getBathroomNames = async () => {
     const res = await Axios.get("http://localhost:3001/bathrooms");
     const bathroomNames = res.data.map((bathroom) => bathroom.name);
     setBathroomsList(bathroomNames);
+    setBathroomsJSON(res.data);
     setFuse(new Fuse(bathroomNames,options));
   }
 
   useEffect(async () => {
     await getBathroomNames();
   }, [])
-
-  useEffect(() => {
-    // console.log(fuse); 
-    console.log('fuse init!');
-  }, [fuse])
   
   const changeHandler = (e) => {
     const val = e.target.value;
     event.preventDefault(); //prevent default behavior
     const similarBathrooms= fuse.search(val);
     setBathroomResults(similarBathrooms);
+  }
+
+  const createCard = (idx) => {
+    const bathroom = bathroomsJSON.filter((data) => data.name===bathroomsList[idx])[0]
+    
+    const reducer = (acc, curr) => acc + curr;
+    const length = bathroom.reviews.length;
+    const reviews = bathroom.reviews.map((e) => e.rating);
+    const avg_rating = (length===0 ? NaN : reviews.reduce(reducer) / length).toFixed(1);
+    
+    return (
+      <SearchCard
+        key={bathroom._id}
+        name={bathroom.name}
+        location={bathroom.longitude.toFixed(3) + ', ' + bathroom.latitude.toFixed(3)}
+        rating = {avg_rating}
+        img={bathroom.img}
+      />
+    )
   }
 
   return (
@@ -65,18 +79,13 @@ const ModalSheet = () => {
                 </div>
                 
                 <div className="button-wrapper">
-                  <button className="cancel-button" onClick={() => setOpen(false)}>cancel</button>
+                  <button className="cancel-button" onClick={() => setOpen(false)}>Cancel</button>
                 </div>
               </div>
 
               <div>
                 {
-                  bathroomResults.map(idx => (
-                    <div key={idx}>
-                      <h1>{bathroomsList[idx]}</h1>
-                      {/* <p>{reviews}</p> */}
-                    </div>
-                  ))
+                  bathroomResults.map((idx) => createCard(idx))
                 }
               </div>
             </div>
---
+++ client/src/components/SearchCard.js
@@ -0,0 +1,26 @@
+import './SearchCard.css';
+import React from 'react';
+import toiletIcon from '../assets/toilet_circle.svg';
+import star from '../assets/star.svg';
+
+const SearchCard = ({ name, location, rating, img }) => {
+    return (
+        <div className="search-card-wrapper">
+            <div className="picture">
+                <img src={toiletIcon} />
+            </div>
+            <div className="text">
+                <div className="name">{name}</div>
+                <div className="details">
+                    <div className="info">
+                        <div className="availability">Open</div>
+                        <div className="address">{location}</div>
+                    </div>
+                    <div className="rating">{rating} <img src={star} /></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchCard;
